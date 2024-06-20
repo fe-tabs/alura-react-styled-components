eac514f99efe14a8e2494790868a48b7d991e658
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-06-20 12:46:23 -0300

    :lipstick: feat: set background

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import { styled } from "styled-components";
 import { Normalize } from 'styled-normalize';
+
+const BackgroundGradient = styled.div`
+  background: linear-gradient(
+    174.61deg,
+    #041833 4.16%,
+    #04244F 48%,
+    #154580 96.76%
+  );
+  min-height: 100vh;
+  width: 100%;
+`;
 
 function App() {
   return (
